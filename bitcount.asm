229a4d9c117aba0a6b4acc7c9cccd78c6cf56e9a
Erick Veil <registry@erickveil.com>
2011-10-13 14:45:34 -0700

    BITCOUNT
    Bit count proc working. Pass a value via the stack, and the number of set
    bits is returned via AL.
    
    Now to Mask for even or odd detection.
    
    Also, an asm discovery: in order to implicitly pull a value from the stack,
    the MODEL must be set up with either C or BASIC setting. I know the C setting
    sets up procs for inclusion as C modules, so I am assuming the BASIC sets up
    for inclusion as a module for the BASIC language.
    
    The actual information on this is supprisingly hard to find. I learned about
    the MODEL C op from a library book. The internet and most books only address
    the memory size parameter of MODEL. I don't understand why when presenting
    MODEL, all parameters are not just given to you.
    
    I plan to track down a difinitive spec sheet for this language.
    Instructional texts are too affraid to overwhelm me with too much
    information. THis approach does not sit with me.

--- bitcount.asm
+++ bitcount.asm
@@ -2,12 +2,15 @@
 
 
 EXTRN	PUTHEX:FAR
+EXTRN	PUTDEC$:FAR
+EXTERN	PUTBIN:FAR
+EXTERN	NEWLINE:FAR
 
 PAGE		80,132
-.MODEL		SMALL
+.MODEL		SMALL, C
 .STACK		64
-.FARDATA
-
+.DATA
+	TEST_WRD	DW	15
 .CODE 
 
 MAIN	PROC	FAR	PUBLIC
@@ -15,16 +18,67 @@ MAIN	PROC	FAR	PUBLIC
 	MOV		AX,	SEG	DGROUP
 	MOV		DS,AX
 	
-	CALL BITCOUNT
+	
+	; print in decimal
+	MOV		AX,TEST_WRD
+	MOV		BH,0
+	CALL	PUTDEC$
+	
+	; Make call
+	MOV		BX,TEST_WRD
+	PUSH	BX
+	CALL 	BITCOUNT
+
+	; the count
+	CALL	NEWLINE
+	MOV		BH,0
+	CALL	PUTDEC$
+	
+	; the number in bin
+	CALL	NEWLINE
+	MOV		BL,1
+	MOV		AX,TEST_WRD
+	CALL	PUTBIN
+	
+	CALL	NEWLINE
+	
+	
 	
 .EXIT
 MAIN	ENDP
 
-BITCOUNT	PROC	NEAR	PUBLIC	USES CX,
-			MONKEY:WORD
-	
+COMMENT*
+	BITCOUNT
+	Erick Veil
+	10-13-11
+	Counts the number of bits in a 16 bit binary word
+	word is passed on the stack
+	Value returned in AL
+*
+BITCOUNT	PROC	NEAR PUBLIC USES CX BX, BINVAL:WORD
+	MOV		BX,BINVAL	; working value
+	MOV		CX,16		; 16 bit count
+	SUB		AX,AX		; holds number of 1s
+	ROTATE:
+		ROL		BX,1
+		JNC		NEXT
+		; if 1
+		INC		AL
+	NEXT:
+		LOOP	ROTATE	
 	RET
 BITCOUNT	ENDP
+
+COMMENT*
+	EVENODD
+	Erick Veil
+	10-13-11
+	Pre: pass a number via the stack
+	Post: returns 0 if even, 1 if odd
+*
+EVENODD		PROC	NEAR PUBLIC	ONESCOUNT:WORD
+	
+EVENODD		ENDP
 
 ; Procs here
 	
