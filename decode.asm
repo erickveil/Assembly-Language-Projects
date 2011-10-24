7ea73e1bfd14508db4bc56ae1f429f780ba6e4f6
Erick Veil <registry@erickveil.com>
2011-10-24 11:27:50 -0700

    decode: masking and setting the bad bit
    The nice thing about modular programming is that I get tu reuse a lot of the
    encode subs with very little modification to decode tha values.

--- decode.asm
+++ decode.asm
@@ -31,12 +31,16 @@ PAGE		80,132
 	MASK_4	DW	0000000011110000B
 	MASK_8	DW	0000000000001111B
 	
-	ROT_1	DW	10
-	ROT_2	DW	9
-	ROT_4	DW	7
+	BADVAL	DW	0000B
+	
+	ROT_1	DW	0
+	ROT_2	DW	1
+	ROT_4	DW	2
 	ROT_8	DW	3
 	
-	MSG_TITLE	DB	'*** decode.asm by Erick Veil ***';34
+	TESTVAL	DW	0000000000000111B
+	
+	MSG_TITLE	DB	'*** decode.asm by Erick Veil ***';32
 	MSG_PROMPT	DB	'Enter number for decoding: ';27
 	MSG_INBIN	DB	'Input value in binary:   ' ;25
 	MSG_INOCT	DB	'Input value in octal:    '
@@ -57,16 +61,115 @@ MAIN	PROC	FAR
 	
 	CALL	HEAD
 	
+	PUSH	TESTVAL
+	PUSH	MASK_8
+	PUSH	ROT_8
+	CALL	MASKING
+	
+	PUSH	TESTVAL
+	PUSH	AX
+	PUSH	BADVAL
+	CALL	REPORT
+	
+	
+	
 .EXIT
 MAIN	ENDP
 
+COMMENT*
+	MASKING
+	Erick Veil
+	10-17-11
+	PRE: Pass via the stack: PACKET as an 11 bit vlaue with data 
+		bits set, BMASK as 11 bit value for the parity mask to be 
+		used, then PSHIFT, which is the number of places to 
+		shift the parity bit in order to set it. 
+	POST: Returns 4 bit value that will deffine the bad bit once
+		all bits are set.
+	parity bits set
+*
+MASKING	PROC	NEAR PUBLIC	PACKET:WORD, BMASK:WORD, PSHIFT:BYTE
+	PUSHF
+	
+	MOV		AX,PACKET
+	AND		AX,BMASK
+	PUSH	AX
+	CALL	PARITY
+	MOV		CL,PSHIFT
+	ROL		AX,CL
+	OR		AX,BADVAL
+	MOV		BADVAL,AX
+	
+	POPF
+	RET
+MASKING	ENDP
+
+COMMENT*
+	PARITY
+	Erick Veil
+	10-16-11
+	PRE: PUSH value to be processed
+	POST: Counts the number of set bits in the value passed
+		and returns wether there is an even or odd number 
+		of them via AX.
+*
+PARITY	PROC	NEAR PUBLIC	BITVAL:WORD
+	PUSHF
+	
+	PUSH	BITVAL
+	CALL	BITCOUNT
+	PUSH	AX
+	CALL	EVENODD
+	
+	POPF
+	RET
+PARITY	ENDP
+
+COMMENT*
+	BITCOUNT
+	Erick Veil
+	10-13-11
+	Counts the number of bits in a 16 bit binary word
+	word is passed on the stack
+	Value returned in AL
+*
+BITCOUNT	PROC	NEAR PUBLIC USES CX BX, BINVAL:WORD
+	PUSHF
+	MOV		BX,BINVAL	; working value
+	MOV		CX,16		; 16 bit count
+	SUB		AX,AX		; holds number of 1s
+	ROTATE:
+		ROL		BX,1
+		JNC		NEXT
+		; if 1
+		INC		AL
+	NEXT:
+		LOOP	ROTATE	
+	POPF
+	RET
+BITCOUNT	ENDP
+
+COMMENT*
+	EVENODD
+	Erick Veil
+	10-13-11
+	Pre: pass a number via the stack
+	Post: returns via AX 0 if even, 1 if odd
+*
+EVENODD		PROC	NEAR PUBLIC	USES CX, ONESCOUNT:WORD
+	PUSHF
+	MOV		AX,ONESCOUNT
+	AND		AX,1
+	POPF
+	RET
+EVENODD		ENDP
 
 HEAD	PROC	NEAR PUBLIC	USES CX 
 	PUSHF
 	
 	CALL	NEWLINE	
 	LEA		DI,MSG_TITLE
-	MOV		CX,34	
+	MOV		CX,32
 	CALL	PUTSTRNG
 	CALL	NEWLINE	
 	CALL	NEWLINE	
@@ -77,5 +180,76 @@ HEAD	PROC	NEAR PUBLIC	USES CX
 	POPF
 	RET
 HEAD	ENDP
+
+COMMENT*
+	REPORT
+	Erick Veil
+	10-17-11
+	PRE: Pass via the stack the input value and the encoded value
+	POST: Outputs a report of the code's process
+*
+REPORT	PROC	NEAR PUBLIC	USES CX BX PACKET:WORD, CODEVAL:WORD, BAD:WORD
+	PUSHF
+	
+	; Input value in binary
+	CALL	NEWLINE	
+	LEA		DI,MSG_INBIN
+	MOV		CX,25
+	CALL	PUTSTRNG	
+	MOV		AX,PACKET
+	MOV		BL,1
+	CALL	PUTBIN
+	LEA		DI,SUFX_BIN
+	MOV		CX,1
+	CALL	PUTSTRNG	
+	
+	; Input Value in Octal
+	CALL	NEWLINE	
+	LEA		DI,MSG_INOCT
+	MOV		CX,25
+	CALL	PUTSTRNG
+	CALL	PUTOCT
+	LEA		DI,SUFX_OCT
+	MOV		CX,1
+	CALL	PUTSTRNG	
+	
+	; bad bit
+	CALL	NEWLINE	
+	LEA		DI,MSG_BAD
+	MOV		CX,25
+	CALL	PUTSTRNG
+	MOV		AX,BAD
+	MOV		BL,0
+	CALL	PUTDEC$
+	
+	; Decoded value binary
+	CALL	NEWLINE	
+	LEA		DI,MSG_OUTBIN
+	MOV		CX,25
+	CALL	PUTSTRNG	
+	MOV		AX,CODEVAL
+	CALL	PUTBIN
+	LEA		DI,SUFX_BIN
+	MOV		CX,1
+	CALL	PUTSTRNG
+
+	;decoded value in octal
+	CALL	NEWLINE	
+	LEA		DI,MSG_OUTOCT
+	MOV		CX,25
+	CALL	PUTSTRNG
+	CALL	PUTOCT
+	LEA		DI,SUFX_OCT
+	MOV		CX,1
+	CALL	PUTSTRNG	
+
+	CALL	NEWLINE	
+	LEA		DI,MSG_PAUSE
+	MOV		CX,26
+	CALL	PAUSE
+	
+	POPF
+	RET
+REPORT	ENDP
 
 END		MAIN
