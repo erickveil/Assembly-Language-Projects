6b8412110aa0a3e5d75e7b6536950310df0dce2c
Erick Veil <registry@erickveil.com>
2011-11-07 14:42:28 -0800

    marbles
    though subscript=0, it looks like array[0] does not return the same value as
    array[subscript]

--- marbles.asm
+++ marbles.asm
@@ -1,12 +1,10 @@
 COMMENT*
-	array.asm
+	marbles.asm
 	Erick Veil
 	10-31-2011
-	
-	Input numeric date and learn the day of the week.
-	An exercise showing the use of simulated arrays
-	using memory offsets in asm
-
+	for each cup looked at, if it is empty, add a marble, if it is full, 
+	take a marble. N=1 on the first iteration, N=2 on the second, etc. 
+	Check every Nth cup until N=500. Observe what cups have marbles.
 *
 
 EXTRN	GETDEC$:FAR
@@ -23,8 +21,8 @@ PAGE		80,132
 .STACK		64
 .FARDATA	DSEG
 	; each bit is a cup. 1 = marble, 0 = empty
-	CUPS	dw	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	CUPS_SZ	dw	6 ; twice the number of word elements in CUPS
+	CUPS	dw	31 dup (0)
+	CUPS_SZ	dw	4 ; twice the number of word elements in CUPS
 	B_MASK	dw	1000000000000000b
 	MSG_PAUSE	db	'Press a key to continue..' ;25
 	REP_VAL	dw	0
@@ -39,8 +37,16 @@ MAIN	PROC	FAR
 	MOV		DS,AX
 	MOV		ES,AX
 	
+	mov		bx,0
+	mov		ax,CUPS[bx]
+	push	ax
+	push	bx
+	call	TOGGLE
+	
+	
 	call	NEWLINE
 	call	REPORT
+	
 	
 	call	NEWLINE
 	lea		di,MSG_PAUSE
@@ -50,6 +56,78 @@ MAIN	PROC	FAR
 	
 .EXIT
 MAIN	ENDP
+COMMENT*
+	TOGGLE
+	Erick Veil
+	11-07-11
+	PRE:	pass 16 bit sequence via stack, 
+		PASS should be set to N, the iteration number 
+		and every Nth cup to check
+	POST: 
+*
+TOGGLE	PROC	NEAR PUBLIC	CUP_WORD:WORD,SUBSCRIPT:WORD
+	PUSHF
+	
+	mov	ax,SUBSCRIPT
+	call	PUTDEC$
+	call	NEWLINE
+	
+	; couter ax = 16/N
+	mov		bx,PASS
+	mov		ax,16
+	mov		dx,0
+	div		bx
+	
+	mov		bx,CUP_WORD
+	
+		
+	toggle_loop:
+		; each iteration, look at a place, flip its bit only
+		xor		bx,B_MASK		
+		
+		; move toggle bit over N
+		mov		cx,PASS
+		ror		B_MASK,cl	; after PASS 127, this will cause problems	
+		
+		; next iteration
+		dec		ax
+		cmp		ax,0
+	jne		toggle_loop
+	; set the changed word back in the array
+		
+		push	ax
+		push	bx
+		mov		ax,bx
+		mov		bl,1
+		call	PUTBIN
+		call	NEWLINE
+		pop		bx
+		pop		ax
+		
+	mov		CUPS[SUBSCRIPT],bx
+		; though SUBSCRIPT = 0..
+		push	ax
+		push	bx
+		mov		ax,CUPS[0]
+		mov		bl,1
+		call	PUTBIN
+		call	NEWLINE
+		pop		bx
+		pop		ax
+		; .. CUPS[SUBSCRIPT] != CUPS[0]
+		push	ax
+		push	bx
+		mov		ax,CUPS[SUBSCRIPT]
+		mov		bl,1
+		call	PUTBIN
+		call	NEWLINE
+		pop		bx
+		pop		ax
+	
+	POPF
+	RET
+	
+TOGGLE	ENDP
 
 COMMENT*
 	REPORT
@@ -107,6 +185,27 @@ REPORT	PROC	NEAR PUBLIC uses ax bx cx dx
 	RET
 	
 REPORT	ENDP
+
+COMMENT*
+	SET_ELEMENT_VAL
+	Erick Veil
+	11-07-11
+	PRE: pass the array subscript via the stack, then pass the 
+	arrray name that starts the memory offset
+	eg: lea dx,ARRAY..push dx
+	Then pass the value to set
+	POST: sets the contents of ARRAY at element SUBSCRIPT to VAL
+*
+SET_ELEMENT_VAL	PROC	NEAR PUBLIC uses di, SUBSCRIPT:WORD, ARRAY:WORD, VAL:WORD
+	PUSHF
+	
+	mov	di,ARRAY
+	add	di,SUBSCRIPT
+	;mov	[di],VAL
+		
+	POPF
+	RET
+SET_ELEMENT_VAL	ENDP
 
 COMMENT*
 	GET_ELEMENT_VAL
